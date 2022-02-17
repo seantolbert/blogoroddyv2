c285b9f496e5c7154e7e94ca54e272e0fcf43c5e
seantolbert <84528717+seantolbert@users.noreply.github.com>
2022-02-17 14:58:16 -0500

    dumped database for a completely new gallery page model without streamfield

--- blogoroddyv2/gallery/models.py
+++ blogoroddyv2/gallery/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import StreamField
-from wagtail.core import blocks
-from wagtail.images.blocks import ImageChooserBlock
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
 
 class GalleryPage(Page):
-    gallery_photo = StreamField([
-        ('image', ImageChooserBlock()),
-        ('description', blocks.CharBlock())
-    ], 
-    blank=True,
-    null=True
+    date = models.DateField(blank=True, null=True)
+
+    gallery_photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
     )
 
+    description = models.CharField(max_length=500, blank=True, null=True)
+
     content_panels = Page.content_panels + [
-        StreamFieldPanel('gallery_photo')
+        FieldPanel('date'),
+        ImageChooserPanel('gallery_photo'),
+        FieldPanel('description')
     ]
 
+class GalleryIndexPage(Page):
+    
+    def posts(self):
+        posts = GalleryPage.objects.live()
+        posts = posts.order_by('-date')
+        return posts
