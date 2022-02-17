4ec9b5316ffde75f796eb8327a92bd4cebd6d4ae
seantolbert <84528717+seantolbert@users.noreply.github.com>
2022-02-17 12:30:09 -0500

    migrates new streamfield model addtion for gallery model

--- blogoroddyv2/gallery/models.py
+++ blogoroddyv2/gallery/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.admin.edit_handlers import StreamFieldPanel
 
 class GalleryPage(Page):
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL
+    gallery_photo = StreamField([
+        ('image', ImageChooserBlock()),
+        ('description', blocks.CharBlock())
+    ], 
+    blank=True,
+    null=True
     )
-    excerpt = models.CharField(max_length=300, blank=True, null=True)
 
     content_panels = Page.content_panels + [
-        ImageChooserPanel('image'),
-        FieldPanel('excerpt')
+        StreamFieldPanel('gallery_photo')
     ]
+
