cb9c2ba8a2674b6d8f6b0db777c8f6a5c45afa04
seantolbert <84528717+seantolbert@users.noreply.github.com>
2022-02-17 17:28:46 -0500

    attempts footer

--- blogoroddyv2/home/models.py
+++ blogoroddyv2/home/models.py
@@ -6,19 +6,20 @@ from wagtail.admin.edit_handlers import FieldPanel
 from blog.models import BlogPage
 from gallery.models import GalleryPage
 
+
 class HomePage(Page):
     headline = models.CharField(max_length=100, blank=True, null=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('headline'),
+        FieldPanel("headline"),
     ]
 
     def blogs(self):
         blogs = BlogPage.objects.live()
-        blogs = blogs.order_by('-date')[:3]
+        blogs = blogs.order_by("-date")[:3]
         return blogs
-    
+
     def photos(self):
         photos = GalleryPage.objects.live()
-        photos = photos.order_by('-date')[:3]
+        photos = photos.order_by("-date")[:3]
         return photos
