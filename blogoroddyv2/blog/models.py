a431e32131742a54b811887d176c5f0b9f6ced75
seantolbert <84528717+seantolbert@users.noreply.github.com>
2022-03-01 21:19:38 -0500

    initiates final blog page styling

--- blogoroddyv2/blog/models.py
+++ blogoroddyv2/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
@@ -7,40 +8,46 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
-class BlogPage(Page):
-    date = models.DateField(blank=True, null=True)
 
-    description = models.CharField(max_length=255, blank = True, null = True)
-    
+class BlogPage(Page):
+    date = models.DateTimeField(default=timezone.now, blank=True, null=True)
+
+    description = models.CharField(max_length=255, blank=True, null=True)
+
     main_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL
+        "wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL
     )
 
     main_image_excerpt = models.CharField(max_length=300, blank=True, null=True)
 
-    content = StreamField(
+    body = StreamField(
         [
-            ('heading', blocks.CharBlock()),
-            ('content', blocks.RichTextBlock()),
-            ('image', ImageChooserBlock())
-        ], 
-        blank=True, null=True
+            (
+                "content",
+                blocks.StructBlock(
+                    [
+                        ("heading", blocks.CharBlock()),
+                        ("content", blocks.RichTextBlock()),
+                        ("image", ImageChooserBlock()),
+                    ]
+                )
+            )
+        ],
+        blank=True,
+        null=True,
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('description'),
-        ImageChooserPanel('main_image'),
-        FieldPanel('main_image_excerpt'),
-        StreamFieldPanel('content'),
+        FieldPanel("date"),
+        FieldPanel("description"),
+        ImageChooserPanel("main_image"),
+        FieldPanel("main_image_excerpt"),
+        StreamFieldPanel("body"),
     ]
 
+
 class BlogIndexPage(Page):
-    
     def blogs(self):
         blogs = BlogPage.objects.live()
-        blogs = blogs.order_by('-date')
+        blogs = blogs.order_by("-date")
         return blogs
